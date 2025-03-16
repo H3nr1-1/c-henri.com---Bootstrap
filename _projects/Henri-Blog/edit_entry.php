b46bad24432cd2ee8f4c7596a7ab5a8a4880395e
Chris <henri@c-henri.com>
2025-03-16 16:35:27 -0500

    meta data changes and some styling changes to home page.
    got rid of redundant meta data on multiple pages, changed the font on home page along with nav background and some placements in header.

--- _projects/Henri-Blog/edit_entry.php
+++ _projects/Henri-Blog/edit_entry.php
@@ -40,13 +40,6 @@ if($_SERVER["REQUEST_METHOD"] == "GET") {
 		<!-- Important Meta data -->
 		<meta http-equiv="Content-Language" content="en">
 		<meta name="description" content="A blog to my journey as a Front-end Web Developer">
-		<meta name="keywords" content="Website Developement, Website Design, Manitowoc, Wisconsin, Web Developement, Blog">
-		<meta name="geo.placename" content="Manitowoc, Wisconsin, USA">
-		<meta name="robots" content="index, follow">
-		<meta property="og:site_name" content="© Chris Henrickson Web Designer">
-		<meta property="og:type" content="website">
-		<meta property="og:title" content="Edit_entry - © Chris Henrickson Web Designer">
-		<meta property="og:url" content="https://www.c-henri.com/edit_entry">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<!-- Bootstrap CDN links -->
 		<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
--- _projects/Henri-Blog/sign_in.php
+++ _projects/Henri-Blog/sign_in.php
@@ -6,13 +6,6 @@
 		<!-- Important Meta data -->
 		<meta http-equiv="Content-Language" content="en">
 		<meta name="description" content="A blog to my journey as a Front-end Web Developer">
-		<meta name="keywords" content="Website Developement, Website Design, Manitowoc, Wisconsin, Web Developement, Blog">
-		<meta name="geo.placename" content="Manitowoc, Wisconsin, USA">
-		<meta name="robots" content="index, follow">
-		<meta property="og:site_name" content="© Chris Henrickson Web Designer">
-		<meta property="og:type" content="website">
-		<meta property="og:title" content="Sign_in - © Chris Henrickson Web Designer">
-		<meta property="og:url" content="https://www.c-henri.com/sign_in">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<!-- Bootstrap CDN links -->
 		<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
